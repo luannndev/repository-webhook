e818ba753da1d106c97662d7bad39f1d6bd30acf
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 14:02:33 +0200

    Add webhook clients and settings initialization
    Enhanced the DiscordWebhookPlugin class to include settings reference and webhook clients initialization. This includes setting up both a root webhook client and a map for multiple webhook clients, and integrating them with the shared configuration facade.

--- src/main/java/dev/luan/repository/DiscordWebhookPlugin.java
+++ src/main/java/dev/luan/repository/DiscordWebhookPlugin.java
@@ -1,16 +1,32 @@
 package dev.luan.repository;
 
+import club.minnced.discord.webhook.WebhookClient;
+import com.reposilite.configuration.shared.SharedConfigurationFacade;
 import com.reposilite.plugin.api.Facade;
 import com.reposilite.plugin.api.Plugin;
 import com.reposilite.plugin.api.ReposilitePlugin;
+import dev.luan.repository.settings.DiscordWebhookSettings;
 import org.jetbrains.annotations.Nullable;
+import panda.std.reactive.MutableReference;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Plugin(name = "discord-webhook-plugin", dependencies = {"configuration", "local-configuration", "shared-configuration"})
 public class DiscordWebhookPlugin extends ReposilitePlugin {
 
 
+    MutableReference<DiscordWebhookSettings> settingsRef;
+
+    WebhookClient rootWebhookClient;
+    Map<String, WebhookClient> webhookClientMap;
+
     @Override
     public @Nullable Facade initialize() {
+        webhookClientMap = new HashMap<>();
+
+        SharedConfigurationFacade sharedConfigurationFacade = extensions().facade(SharedConfigurationFacade.class);
+        sharedConfigurationFacade.updateSharedSettings("discord-webhook", new DiscordWebhookSettings());
         return null;
     }
 }
